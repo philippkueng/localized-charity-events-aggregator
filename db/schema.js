001b7cb5d5ca46248bcbd17270e665593d9aee4c
Ivan Krechetov <ivan.krechetov@gmail.com>
2011-12-03 16:19:17 +0100

    Delete default User model generated by rw init

--- db/schema.js
+++ db/schema.js
@@ -1,9 +1,3 @@
-define('User', function () {
-    property('email', String, { index: true });
-    property('password', String);
-    property('activated', Boolean, {default: false});
-});
-
 var Event = describe('Event', function () {
     property('title', String);
     property('location', String);

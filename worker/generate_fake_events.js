2f5b162b80cce42ae8243d6ec4fc30d8c3c93b1c
Ivan Krechetov <ivan.krechetov@gmail.com>
2011-12-04 15:08:20 +0100

    A generate_fake_events.js script which works, but never terminates :)

---
+++ worker/generate_fake_events.js
@@ -0,0 +1,30 @@
+var Faker = require('Faker'),
+    _ = require('underscore');
+
+var randomInt = function (min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+
+var mongodb = require('mongodb');
+var server = new mongodb.Server("127.0.0.1", 27017, {});
+
+new mongodb.Db('localized-charity-events-aggregator-dev', server, {}).open(function (error, client) {
+    if (error) throw error;
+        var collection = new mongodb.Collection(client, 'events');
+
+        _(100).times(function () {
+            collection.insert(
+                {
+                    title: Faker.Lorem.sentence(),
+                    location: Faker.Address.city(),
+                    description: Faker.Lorem.paragraphs() + "\n" + 'http://' + Faker.Internet.domainName(),
+                    startDate: new Date(randomInt(2009, 2012), randomInt(0, 11), randomInt(1, 28), randomInt(12, 20)),
+                    duration: "about " + randomInt(2, 4) + " hours"
+                },
+
+                {}
+            );
+        });
+});
+

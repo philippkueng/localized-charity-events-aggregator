4489eb524e64028570886eecc5e865cfe8c48b18
Ivan Krechetov <ivan.krechetov@gmail.com>
2011-12-04 15:26:21 +0100

    Indentation

--- worker/generate_fake_events.js
+++ worker/generate_fake_events.js
@@ -17,20 +17,21 @@ var server = new mongodb.Server("127.0.0.1", 27017, {});
 
 new mongodb.Db('localized-charity-events-aggregator-dev', server, {}).open(function (error, client) {
     if (error) throw error;
-        var collection = new mongodb.Collection(client, 'events');
 
-        _(100).times(function () {
-            collection.insert(
-                {
-                    title: capitalize(Faker.Lorem.sentence()),
-                    location: Faker.Address.city(),
-                    description: Faker.Lorem.paragraphs() + "\n\n" + 'http://' + Faker.Internet.domainName(),
-                    startDate: new Date(randomInt(2009, 2012), randomInt(0, 11), randomInt(1, 28), randomInt(12, 20)),
-                    duration: "about " + randomInt(2, 4) + " hours"
-                },
+    var collection = new mongodb.Collection(client, 'events');
 
-                {}
-            );
-        });
+    _(100).times(function () {
+        collection.insert(
+            {
+                title: capitalize(Faker.Lorem.sentence()),
+                location: Faker.Address.city(),
+                description: Faker.Lorem.paragraphs() + "\n\n" + 'http://' + Faker.Internet.domainName(),
+                startDate: new Date(randomInt(2009, 2012), randomInt(0, 11), randomInt(1, 28), randomInt(12, 20)),
+                duration: "about " + randomInt(2, 4) + " hours"
+            },
+
+            {}
+        );
+    });
 });
 

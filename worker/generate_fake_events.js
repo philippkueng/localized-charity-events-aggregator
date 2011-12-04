69048a50146ba12bd2cea294988650076c411f56
Ivan Krechetov <ivan.krechetov@gmail.com>
2011-12-04 15:16:10 +0100

    Capitalize the generated events' titles

--- worker/generate_fake_events.js
+++ worker/generate_fake_events.js
@@ -5,6 +5,12 @@ var randomInt = function (min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
+var capitalize = function (s) {
+    var res = s.trim();
+
+    if (res.length <= 0) return '';
+    return res[0].toUpperCase() + res.substr(1);
+};
 
 var mongodb = require('mongodb');
 var server = new mongodb.Server("127.0.0.1", 27017, {});
@@ -16,9 +22,9 @@ new mongodb.Db('localized-charity-events-aggregator-dev', server, {}).open(funct
         _(100).times(function () {
             collection.insert(
                 {
-                    title: Faker.Lorem.sentence(),
+                    title: capitalize(Faker.Lorem.sentence()),
                     location: Faker.Address.city(),
-                    description: Faker.Lorem.paragraphs() + "\n" + 'http://' + Faker.Internet.domainName(),
+                    description: Faker.Lorem.paragraphs() + "\n\n" + 'http://' + Faker.Internet.domainName(),
                     startDate: new Date(randomInt(2009, 2012), randomInt(0, 11), randomInt(1, 28), randomInt(12, 20)),
                     duration: "about " + randomInt(2, 4) + " hours"
                 },
